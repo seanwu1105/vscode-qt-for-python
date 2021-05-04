e567d1822e8ad3bfe09595b88b2d252689aa5a5d
Sean Wu <seanwu1105@gmail.com>
2021-05-04 23:07:36 +0800

    Fix: VSCE package errors.

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
-import { name, publisher } from '../package.json';
+const PUBLISHER = 'seanwu'; // Should be the same in package.json
+
+const NAME = 'vscode-qt-for-python'; // Should be the same in package.json
 
 export const EXTENSION_NAME = 'qtForPython';
 
-export const EXTENSION_ID = `${publisher}.${name}`;
+export const EXTENSION_ID = `${PUBLISHER}.${NAME}`;
 
 export type SupportedTool = 'designer' | 'lupdate' | 'uic' | 'rcc';
