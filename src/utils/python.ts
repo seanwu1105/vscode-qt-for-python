ae6436f23d477cd02606cb461b13e222bf38d379
Shuang Wu <seanwu1105@gmail.com>
2021-12-02 00:05:48 -0500

    Use Python extension public API to get Python path
    Fix #151.
    Fix #153.

--- src/utils/python.ts
+++ src/utils/python.ts
@@ -1,27 +1,25 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import * as vscode from 'vscode';
-import { getActiveWorkspaceFolderPath, getExtensionPath } from './paths';
+import { getExtensionPath } from './paths';
 
 export async function resolvePythonScript(fileName: string) {
-  return `"${getPythonPath()}" "${getExtensionPath()}/python/scripts/${fileName}.py"`;
+  return `"${await getPythonInterpreterPath()}" "${getExtensionPath()}/python/scripts/${fileName}.py"`;
 }
 
-function getPythonPath() {
-  const pythonPath =
-    vscode.workspace.getConfiguration('python').get<string>('pythonPath') ??
-    'python';
-
-  // assume pythonPath is relative to workspace root.
-  const activeWorkspaceFolderPath = getActiveWorkspaceFolderPath();
-  if (!activeWorkspaceFolderPath) return pythonPath;
-  const relativePath = path.join(activeWorkspaceFolderPath, pythonPath);
-  if (fs.existsSync(relativePath)) return relativePath;
-  return pythonPath;
+async function getPythonInterpreterPath(): Promise<string> {
+  // Get path with the Python extension public API: https://github.com/microsoft/vscode-python/blob/main/src/client/api.ts#L135
+  const pythonExtensionApi = await vscode.extensions
+    .getExtension('ms-python.python')
+    ?.activate();
+  const pythonExecCommand =
+    pythonExtensionApi?.settings.getExecutionDetails().execCommand;
+  if (pythonExecCommand) return pythonExecCommand[0];
+  return 'python';
 }
 
 export async function installPythonPackage(pypiPackageName: string) {
   const terminal = vscode.window.createTerminal();
   terminal.show();
-  terminal.sendText(`"${getPythonPath()}" -m pip install ${pypiPackageName}`);
+  terminal.sendText(
+    `"${await getPythonInterpreterPath()}" -m pip install ${pypiPackageName}`
+  );
 }
