5d82e7d943149d1716bb4ad6767f28199b307504
Shuang Wu <seanwu1105@gmail.com>
2021-12-02 16:21:52 -0500

    Use Python extension public API to get Python path (#168)
    * Use Python extension public API to get Python path
    
    Fix #151.
    Fix #153.
    
    * Use `python.defaultInterpreterPath` as fallback.
    
    Throw error when no Python interpreter can be obtained from the
    ms-python extension.

--- src/utils/python.ts
+++ src/utils/python.ts
@@ -1,27 +1,32 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import * as vscode from 'vscode';
-import { getActiveWorkspaceFolderPath, getExtensionPath } from './paths';
+import { NotFoundError } from './error';
+import { getExtensionPath } from './paths';
 
 export async function resolvePythonScript(fileName: string) {
-  return `"${getPythonPath()}" "${getExtensionPath()}/python/scripts/${fileName}.py"`;
+  return `"${await getPythonInterpreterPath()}" "${getExtensionPath()}/python/scripts/${fileName}.py"`;
 }
 
-function getPythonPath() {
-  const pythonPath =
-    vscode.workspace.getConfiguration('python').get<string>('pythonPath') ??
-    'python';
-
-  // assume pythonPath is relative to workspace root.
-  const activeWorkspaceFolderPath = getActiveWorkspaceFolderPath();
-  if (!activeWorkspaceFolderPath) return pythonPath;
-  const relativePath = path.join(activeWorkspaceFolderPath, pythonPath);
-  if (fs.existsSync(relativePath)) return relativePath;
-  return pythonPath;
+async function getPythonInterpreterPath(): Promise<string> {
+  // Get path with the Python extension public API: https://github.com/microsoft/vscode-python/blob/main/src/client/api.ts#L135
+  const pythonExtensionApi = await vscode.extensions
+    .getExtension('ms-python.python')
+    ?.activate();
+  const pythonExecCommand =
+    pythonExtensionApi?.settings.getExecutionDetails().execCommand;
+  if (pythonExecCommand) return pythonExecCommand[0];
+  const pythonDefaultInterpreter = vscode.workspace
+    .getConfiguration('python')
+    .get<string>('defaultInterpreterPath');
+  if (pythonDefaultInterpreter) return pythonDefaultInterpreter;
+  throw new NotFoundError(
+    'Python interpreter cannot could not be obtained from the Python extension.'
+  );
 }
 
 export async function installPythonPackage(pypiPackageName: string) {
   const terminal = vscode.window.createTerminal();
   terminal.show();
-  terminal.sendText(`"${getPythonPath()}" -m pip install ${pypiPackageName}`);
+  terminal.sendText(
+    `"${await getPythonInterpreterPath()}" -m pip install ${pypiPackageName}`
+  );
 }
