6b044bac3b7017d66d178f2cf98bdaeff7061dd0
Shuang Wu <seanwu1105@gmail.com>
2022-08-14 22:56:57 -0500

    refactor: make run fully typed

--- src/run.ts
+++ src/run.ts
@@ -1,23 +1,33 @@
-import { exec } from 'child_process'
-import { promisify } from 'util'
-import type { ErrorResult, SuccessResult } from './result-types'
+import type { ExecException } from 'node:child_process'
+import { exec } from 'node:child_process'
+import type { SuccessResult } from './result-types'
+import { notNil } from './utils'
 
-export async function run({ command, cwd }: RunArgs): Promise<RunResult> {
-  try {
-    const { stdout, stderr } = await promisify(exec)(command.join(' '), { cwd })
-    if (stderr) return { kind: 'StdErrError', message: stderr }
-    return { kind: 'Success', value: stdout }
-  } catch (e) {
-    return { kind: 'ExecutionError', message: JSON.stringify(e) }
-  }
+export async function run({ command }: RunArgs): Promise<RunResult> {
+  return new Promise<RunResult>(resolve => {
+    exec(command.join(' '), (error, stdout, stderr) => {
+      if (notNil(error)) resolve({ kind: 'ExecError', error, stdout, stderr })
+      if (stderr.length !== 0) resolve({ kind: 'StdErrError', stdout, stderr })
+      resolve({ kind: 'Success', value: stdout })
+    })
+  })
 }
 
 type RunArgs = {
   command: readonly string[]
-  cwd: string
 }
 
-type RunResult =
-  | SuccessResult<string>
-  | ErrorResult<'StdErr'>
-  | ErrorResult<'Execution'>
+type RunResult = SuccessResult<string> | ExecError | StdErrError
+
+export type ExecError = {
+  readonly kind: 'ExecError'
+  readonly error: ExecException
+  readonly stdout: string
+  readonly stderr: string
+}
+
+export type StdErrError = {
+  readonly kind: 'StdErrError'
+  readonly stdout: string
+  readonly stderr: string
+}
