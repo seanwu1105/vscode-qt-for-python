8ded5384728c7160a065fd5170dc0a0332db912d
Shuang Wu <seanwu1105@gmail.com>
2023-02-03 14:51:23 -0500

    fix: ensure the observable does not complete before the language client disposed

--- src/qmlls/client.ts
+++ src/qmlls/client.ts
@@ -1,5 +1,14 @@
 import type { Observable } from 'rxjs'
-import { concatMap, defer, of, using } from 'rxjs'
+import {
+  concatMap,
+  defer,
+  of,
+  pairwise,
+  ReplaySubject,
+  startWith,
+  switchMap,
+  using,
+} from 'rxjs'
 import type { OutputChannel } from 'vscode'
 import type {
   LanguageClientOptions,
@@ -22,13 +31,13 @@ export function registerQmlLanguageServer$({
 }: RegisterQmlLanguageServerArgs): Observable<RegisterQmlLanguageServerResult> {
   let client: LanguageClient | undefined = undefined
 
+  const client$ = new ReplaySubject<LanguageClient | undefined>(1)
+
   return getEnabledFromConfig$({ tool: 'qmlls', resource: undefined }).pipe(
-    concatMap(enabled => {
+    switchMap(enabled => {
       if (!enabled)
         return defer(async () => {
-          await client?.dispose()
-          client = undefined
-
+          client$.next(undefined)
           return { kind: 'Success', value: 'qmlls is disabled' } as const
         })
 
@@ -40,26 +49,34 @@ export function registerQmlLanguageServer$({
         concatMap(result => {
           if (result.kind !== 'Success') return of(result)
 
-          return defer(async () => {
-            await client?.dispose()
-          }).pipe(
-            concatMap(() =>
-              using(
-                () => {
-                  client = createClient({
-                    command: result.value.command,
-                    options: result.value.options,
-                    outputChannel,
-                  })
+          return using(
+            () => {
+              client = createClient({
+                command: result.value.command,
+                options: result.value.options,
+                outputChannel,
+              })
+              client$.next(client)
 
-                  return { unsubscribe: () => client?.dispose() }
-                },
-                async () => {
-                  await client?.start()
-                  return { kind: 'Success', value: 'qmlls is enabled' } as const
-                },
+              return {
+                unsubscribe: () => client?.dispose(),
+              }
+            },
+            () =>
+              client$.pipe(
+                stopPreviousClient(),
+                concatMap(client =>
+                  defer(async () => {
+                    await client?.start()
+                    return {
+                      kind: 'Success',
+                      value: `qmlls is enabled with latest config: ${JSON.stringify(
+                        result.value,
+                      )}`,
+                    } as const
+                  }),
+                ),
               ),
-            ),
           )
         }),
       )
@@ -75,6 +92,17 @@ type RegisterQmlLanguageServerArgs = {
 type RegisterQmlLanguageServerResult =
   | ErrorResult<'NotFound'>
   | SuccessResult<string>
+
+// TODO: Unit test this behavior.
+function stopPreviousClient() {
+  return (source$: Observable<LanguageClient | undefined>) =>
+    source$.pipe(
+      startWith(undefined),
+      pairwise(),
+      concatMap(async ([previous]) => previous?.dispose()),
+      concatMap(() => source$),
+    )
+}
 
 function createClient({ command, options, outputChannel }: CreateClientArgs) {
   const serverOptions: ServerOptions = {
